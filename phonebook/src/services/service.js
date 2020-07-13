2cdd9b0ffd15b9e446b026ced3be2a6c4e6d46d0
vanntile <vanntile@protonmail.com>
2020-07-13 11:11:57 +0300

    part3 ex3.9 done

--- phonebook/src/services/service.js
+++ phonebook/src/services/service.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const URL = 'http://localhost:3001/persons'
+const URL = '/api'
 
-const getAll = () => axios.get(`${URL}`).then((response) => response.data)
-const addNew = (newPerson) => axios.post(`${URL}`, newPerson).then((response) => response.data)
-const remove = (id) => axios.delete(`${URL}/${id}`).then((response) => response.data)
-const update = (id, data) => axios.put(`${URL}/${id}`, data).then((response) => response.data)
+const getAll = () => axios.get(`${URL}/persons`).then((response) => response.data)
+const addNew = (newPerson) => axios.post(`${URL}/persons`, newPerson).then((response) => response.data)
+const remove = (id) => axios.delete(`${URL}/person/${id}`).then((response) => response.data)
+const update = (id, data) => axios.put(`${URL}/person/${id}`, data).then((response) => response.data)
 
 export default { getAll, addNew, remove, update }
