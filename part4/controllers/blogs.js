491b404b84654f66bf414e716b946547d5472a75
vanntile <vanntile@protonmail.com>
2020-07-18 11:34:53 +0300

    part 4c done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,18 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
+  const user = await User.findById(body.userId)
+
+  const blog = new Blog({
+    title: body.title || '',
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
   })
+
+  const saved = await blog.save()
+
+  user.blogs = user.blogs.concat(saved._id)
+  await user.save()
+
+  response.json(saved)
 })
 
 module.exports = blogsRouter
