36605629836e565282cbd3d5cd9b2660b0c8e379
vanntile <vanntile@protonmail.com>
2020-07-18 12:35:02 +0300

    part 4 done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+const { request } = require('express')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -11,7 +14,11 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(request.token, config.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title || '',
@@ -27,6 +34,22 @@ blogsRouter.post('/', async (request, response) => {
   await user.save()
 
   response.json(saved)
+})
+
+blogsRouter.delete('/', async (request, response) => {
+  const decodedToken = jwt.verify(request.token, config.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  const blogPost = await Blog.find({ id: request.params.id })
+
+  if (user._id !== blogPost.userId) {
+    response.status(403).end()
+  }
+
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
